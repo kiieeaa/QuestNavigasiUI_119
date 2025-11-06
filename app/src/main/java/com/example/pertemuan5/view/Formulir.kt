5d989af9180ec7dea65a0085642a5b9db53e74ff
kiieeaa <kiyaadinda5@gmail.com>
2025-11-06 17:22:16 +0700

    tambahkan pilihan jenis kelamin menggunakan RadioButton

--- app/src/main/java/com/example/pertemuan5/view/Formulir.kt
+++ app/src/main/java/com/example/pertemuan5/view/Formulir.kt
@@ -46,4 +46,19 @@ fun FormIsian(
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
         ){
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .width(width = 250.dp),
+                label = {Text(text = "Nama Lengkap")},
+                onValueChange = {},
+            )
+
+            HorizontalDivider(modifier = Modifier
+                .padding(all = 20.dp)
+                .width(width = 250.dp), thickness = 1.dp,color =
+                Color.Red)
+        }
         }
