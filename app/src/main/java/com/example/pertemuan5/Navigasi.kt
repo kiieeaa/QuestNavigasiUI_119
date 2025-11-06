a9af9da33e1246d4c07607b976594d5f30cb195a
kiieeaa <kiyaadinda5@gmail.com>
2025-11-06 17:52:22 +0700

    tambahkan enum Navigasi untuk mendefinisikan rute (Formulirku & Detail)

---
+++ app/src/main/java/com/example/pertemuan5/Navigasi.kt
@@ -0,0 +1,23 @@
+package com.example.pertemuan5
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.pertemuan5.view.FormIsian
+import com.example.pertemuan5.view.TampilData
+
+enum class Navigasi {
+    Formulirku,
+    Detail
+}
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier
+) {
